cb42fe5e15c2ab354429a1025a3a589667e44122
ebin-ahm <edieusdi@gmail.com>
2026-01-22 19:29:42 +0800

    style: build get_next_line infrastructure

---
+++ get_next_line.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ebin-ahm <ebin-ahm@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/22 17:48:49 by ebin-ahm          #+#    #+#             */
+/*   Updated: 2026/01/22 19:03:13 by ebin-ahm         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdarg.h>
+# include <unistd.h>
+# include <stddef.h>
+
+size_t	ft_strlen(const char *s);
+char	*ft_strchr(const char *s, int c);
+
+#endif 
---
+++ get_next_line_utils.c
@@ -0,0 +1,69 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ebin-ahm <ebin-ahm@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/22 17:48:54 by ebin-ahm          #+#    #+#             */
+/*   Updated: 2026/01/22 19:19:27 by ebin-ahm         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+size_t	ft_strlen(const char *s)
+{
+	size_t	len;
+
+	len = 0;
+	if (!s)
+		return (0);
+	while (s[len])
+		len++;
+	return (len);
+}
+
+char	*ft_strchr(const char *s, int c)
+{
+	if (!s)
+		return (NULL);
+	while (*s)
+	{
+		if (*s == (char)c)
+			return ((char *)s);
+		s++;
+	}
+	if ((char)c == '\0')
+		return ((char *)s);
+	return (NULL);
+}
+
+char	*ft_strjoin(const char *s1, const char *s2)
+{
+	size_t	len1;
+	size_t	len2;
+	size_t	index1;
+	size_t	index2;
+	char	*out;
+
+	len1 = ft_strlen(s1);
+	len2 = ft_strlen(s2);
+	out = (char *)malloc(len1 + len2 + 1);
+	if (!out)
+		return (NULL);
+	index1 = 0;
+	while (s1 && index1 < len1)
+	{
+		out[index1] = s1[index1];
+		index1++;
+	}
+	index2 = 0;
+	while (s2 && index2 < len2)
+	{
+		out[index1 + index2] = s2[index2];
+		index2++;
+	}
+	out[index1 + index2] = '\0';
+	return (out);
+}
