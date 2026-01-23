f04f547d1ba0b2fda7a9db207b4391c410967c64
ebin-ahm <edieusdi@gmail.com>
2026-01-24 06:03:39 +0800

    Implement extract_line and update_stash functions
    Added functions to extract and update lines from stash.

--- get_next_line.c
+++ get_next_line.c
@@ -10,3 +10,49 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "get_next_line.h"
+
+char	*extract_line(const char *stash)
+{
+	size_t	index;
+
+	if (!stash || stash[0] == '\0')
+		return (NULL);
+	index = 0;
+	while (stash[index] && stash[index] != '\n')
+		index++;
+	if (stash[index] == '\n')
+		return (ft_substr(stash, 0, index + 1));
+	return (ft_strdup(stash));
+}
+
+char	*update_stash(char *stash)
+{
+	size_t	index;
+	char	*new_stash;
+
+	if (!stash)
+		return (NULL);
+	index = 0;
+	while (stash[index] && stash[index] != '\n')
+		index++;
+	if (stash[index] == '\0')
+	{
+		free(stash);
+		return (NULL);
+	}
+	new_stash = ft_substr(stash, index + 1, ft_strlen(stash) - (index + 1));
+	free(stash);
+
+	if (!new_stash || new_stash[0] == '\0')
+	{
+		free(new_stash);
+		return ((NULL));
+	}
+	return (new_stash);
+}
+
+char	*get_next_line(int fd)
+{
+
+}
