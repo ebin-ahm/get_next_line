1740f86e2f3c9b559df5e793dfb68c5c5b1c2ad1
ebin-ahm <edieusdi@gmail.com>
2026-01-27 16:16:34 +0800

    Add new includes and function prototypes to header

--- get_next_line.h
+++ get_next_line.h
@@ -13,15 +13,24 @@
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-# include <stdarg.h>
 # include <unistd.h>
 # include <stddef.h>
+# include <stdlib.h>
+# include <fcntl.h>
+# include <stdio.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 1
+# endif
 
 size_t	ft_strlen(const char *s);
 char	*ft_strchr(const char *s, int c);
 char	*ft_strjoin(const char *s1, const char *s2);
 char	*ft_strdup(const char *s);
+char	*ft_substr(const char *s, unsigned int start, size_t len);
 char	*extract_line(const char *stash);
 char	*update_stash(char *stash);
+char	*read_to_stash(int fd, char *stash);
+char	*get_next_line(int fd);
 
-#endif 
+#endif
